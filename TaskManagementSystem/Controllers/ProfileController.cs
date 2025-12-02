bcaa9a8a7ba0b3a33c60844d5da43c244b4cf000
FirozAhmedSW <firoz.swpr@gmail.com>
2025-12-02 23:02:07 +0600

    add profile and setting code

---
+++ TaskManagementSystem/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;   // Change namespace if needed
+using Microsoft.AspNetCore.Http;
+using TaskManagementSystem.DataContext;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userName = HttpContext.Session.GetString("UserName");
+
+            if (string.IsNullOrEmpty(userName))
+                return RedirectToAction("Login", "Account");
+
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.UserName == userName && !u.IsDeleted);
+
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            return View(user);
+        }
+    }
+}
---
+++ TaskManagementSystem/Controllers/SettingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class SettingController : Controller
+    {
+        // GET: Settings
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+    }
+}
